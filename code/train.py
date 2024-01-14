824f0852316bd19730de5fdd5119944371d91e45
3dida <44850191+aallali@users.noreply.github.com>
2024-01-14 20:15:24 +0100

    [+] : config trainer to run LDA only or all pipelines : @train.py

--- code/train.py
+++ code/train.py
@@ -17,30 +17,32 @@ from sklearn.linear_model import LogisticRegression
 matplotlib.use('TkAgg')
 
 
-def pipeline_creation(X, y, transformer1, transformer2, transformer3):
+def pipeline_creation(X, y, transformer1, transformer2=None, transformer3=None):
     cv = ShuffleSplit(10, test_size=0.2, random_state=42)
 
     lda = LDA(solver='svd')
     log_reg = LogisticRegression(penalty='l1', solver='liblinear', multi_class='auto')
     rfc = RandomForestClassifier(n_estimators=100, random_state=42)
 
-    scores1 = []
-    scores2 = []
-    scores3 = []
+    final_result = []
 
     pipeline1 = make_pipeline(transformer1, lda)
-    pipeline2 = make_pipeline(transformer2, log_reg)
-    pipeline3 = make_pipeline(transformer3, rfc)
-
     scores1 = cross_val_score(pipeline1, X, y, cv=cv, n_jobs=1)
-    scores2 = cross_val_score(pipeline2, X, y, cv=cv, n_jobs=1)
-    scores3 = cross_val_score(pipeline3, X, y, cv=cv, n_jobs=1)
+    final_result.append(('LDA ', pipeline1, scores1))
+    if transformer2:
+        pipeline2 = make_pipeline(transformer2, log_reg)
+        scores2 = cross_val_score(pipeline2, X, y, cv=cv, n_jobs=1)
+        final_result.append(('LOGR', pipeline2, scores2))
+    if transformer3:
+        pipeline3 = make_pipeline(transformer3, rfc)
+        scores3 = cross_val_score(pipeline3, X, y, cv=cv, n_jobs=1)
+        final_result.append(('RFC', pipeline3, scores3))
 
     # print(f"LinearDiscriminantAnalysis : accuracy {scores1.mean().round(2)}, std: {scores1.std().round(2)}")
     # print(f"LogisticRegression         : accuracy {scores2.mean().round(2)}, std: {scores2.std().round(2)}")
     # print(f"RandomForestClassifier     : accuracy {scores3.mean().round(2)}, std: {scores3.std().round(2)}")
 
-    return [('LDA ', pipeline1, scores1), ('LOGR', pipeline2, scores2), ('RFC ', pipeline3, scores3)]
+    return final_result
 
 
 def save_pipeline(pipe, epochs_data_train, labels, subjectID):
@@ -51,26 +53,34 @@ def save_pipeline(pipe, epochs_data_train, labels, subjectID):
     return
 
 
-def train_data(X, y, transformer="CSP"):
+def train_data(X, y, transformer="CSP", run_all_pipelines=False):
     if transformer == "CSP":
         from mne.decoding import CSP
         # using CSP transformers
-        csp1 = CSP()
-        csp2 = CSP()
-        csp3 = CSP()
-        return pipeline_creation(X, y, csp1, csp2, csp3)
+        if run_all_pipelines:
+            csp1 = CSP()
+            csp2 = CSP()
+            csp3 = CSP()
+            return pipeline_creation(X, y, csp1, csp2, csp3)
+        return pipeline_creation(X, y, CSP())
     elif transformer == "FAST_CSP":
         from CSP import CSP
         # using custom CSP transformers
-        csp1 = CSP()
-        csp2 = CSP()
-        csp3 = CSP()
-        return pipeline_creation(X, y, csp1, csp2, csp3)
+        if run_all_pipelines:
+            csp1 = CSP()
+            csp2 = CSP()
+            csp3 = CSP()
+            return pipeline_creation(X, y, csp1, csp2, csp3)
+        return pipeline_creation(X, y, CSP())
     elif transformer == "SPoC":
         # using Spoc transformers
-        Spoc1 = SPoC(n_components=15, reg='oas', log=True, rank='full')
-        Spoc2 = SPoC(n_components=15, reg='oas', log=True, rank='full')
-        Spoc3 = SPoC(n_components=15, reg='oas', log=True, rank='full')
-        return pipeline_creation(X, y, Spoc1, Spoc2, Spoc3)
+
+        if run_all_pipelines:
+            Spoc1 = SPoC(n_components=15, reg='oas', log=True, rank='full')
+            Spoc2 = SPoC(n_components=15, reg='oas', log=True, rank='full')
+            Spoc3 = SPoC(n_components=15, reg='oas', log=True, rank='full')
+            return pipeline_creation(X, y, Spoc1, Spoc2, Spoc3)
+        else:
+            return pipeline_creation(X, y, SPoC(n_components=15, reg='oas', log=True, rank='full'))
     else:
         raise ValueError(f"Unknown transformer, please enter valid one.")
