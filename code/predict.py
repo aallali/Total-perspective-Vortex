9a741865a53dba39ee5d72922e384c9df898092a
3dida <44850191+aallali@users.noreply.github.com>
2024-01-14 21:15:33 +0100

    [+] : custom+conditional logs for prediction : @predict.py

--- code/predict.py
+++ code/predict.py
@@ -1,18 +1,22 @@
 from joblib import load
 import numpy as np
 
-def predict(X, y , subjectId):
-    PREDICT_MODEL = f"models/model-subject-{subjectId}.joblib"
+def predict(X, y , subjectId, experiment_name, log=False):
+    PREDICT_MODEL = f"models/model_subject_{subjectId}_{experiment_name}.joblib"
     try:
         clf = load(PREDICT_MODEL)
     except FileNotFoundError as e:
         raise Exception(f"File not found: {PREDICT_MODEL}")
 
     scores = []
+    print("epoch_nb =  [prediction]    [truth]    equal?")
+    print("---------------------------------------------")
     for n in range(X.shape[0]):
-        pred = clf.predict(X[n:n + 1, :, :])
-        # print(f"epoch_{n} =", "[{:<2}]".format(pred[0]), "truth =", "[{:<2}]".format(y[n:n + 1][0]))
-        scores.append(1 - np.abs(pred[0] - y[n:n + 1][0]))
+        pred = clf.predict(X[n:n + 1, :, :])[0]
+        truth = y[n:n + 1][0]
+        if log:
+            print(f"epoch_{n:2} =      [{pred}]           [{truth}]      {'' if pred == truth else False}")
+        scores.append(1 - np.abs(pred - y[n:n + 1][0]))
     # print("Mean acc= ", str(np.mean(scores).round(2)*100) + "%")
     return np.mean(scores).round(3)
 
