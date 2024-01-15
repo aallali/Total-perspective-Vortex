fb2395433b2495cb02886cedc37ffd9973181a5e
3dida <44850191+aallali@users.noreply.github.com>
2024-01-15 17:28:28 +0100

    [feat] : document some methods : @CSP.py

--- code/CSP.py
+++ code/CSP.py
@@ -17,50 +17,99 @@ class CSP(BaseEstimator, TransformerMixin):
         self.mean = None
         self.std = None
 
-    def _decompose_covs(self, covs):
-        """
-         Return the eigenvalues and eigenvectors of a complex Hermitian ( conjugate symmetric )
-
-        :param covs:
-        :return:
-        """
-        from scipy import linalg
-        n_classes = len(covs)
-        if n_classes == 2:
-            eigen_values, eigen_vectors = linalg.eigh(covs[0], covs.sum(0))
-        else:
-            raise Exception("Not Handled.")
-        return eigen_vectors, eigen_values
-
     def calculate_cov_(self, X, y):
+        """
+        Calculate the covariance matrices for each class.
+
+        Parameters:
+        - X: ndarray, shape (n_epochs, n_channels, n_times)
+            The EEG data.
+        - y: array, shape (n_epochs,)
+            The labels for each epoch.
+
+        Returns:
+        - covs: ndarray, shape (n_classes, n_channels, n_channels)
+            List of covariance matrices for each class.
+        """
+
         _, n_channels, _ = X.shape
         covs = []
 
+        # Iterate over each class
         for l in self.n_classes:
+            # Select epochs corresponding to the current class
             lX = X[np.where(y == l)]
+            # Transpose to have shape (n_channels, n_epochs, n_times)
             lX = lX.transpose([1, 0, 2])
+            # Reshape to (n_channels, -1)
             lX = lX.reshape(n_channels, -1)
-            covs.append(np.cov(lX))
+            # Calculate covariance matrix for the class
+            # The covariance matrix is a square matrix \
+            #   where each element represents the covariance \
+            #   between two corresponding channels (features) in the input data.
+            covs.append(np.cov(lX)) # type: 'numpy.ndarray'
+
         return np.asarray(covs)
 
     def calculate_eig_(self, covs):
+        """
+        Calculate eigenvalues and eigenvectors for pairwise combinations of covariance matrices.
+
+        Parameters:
+        -----------
+        covs : list of 2D arrays
+            List of covariance matrices for different classes.
+
+        Returns:
+        --------
+        tuple
+            Tuple containing lists of eigenvalues and eigenvectors.
+
+        """
         eigenvalues, eigenvectors = [], []
+
+        # Iterate over each covariance matrix
         for idx, cov in enumerate(covs):
+            # Iterate over remaining covariance matrices to create pairwise combinations
             for iidx, compCov in enumerate(covs):
-                if idx < iidx:
-                    [eigVals, eigVects] = linalg.eig(cov, cov + compCov)
+                if idx < iidx:  # Consider each pair only once
+                    # Solve the generalized eigenvalue problem
+                    eigVals, eigVects = linalg.eig(cov, cov + compCov)
+                    # Sort eigenvalues in descending order
                     sorted_indices = np.argsort(np.abs(eigVals - 0.5))[::-1]
+                    # Store sorted eigenvalues and corresponding eigenvectors
                     eigenvalues.append(eigVals[sorted_indices])
                     eigenvectors.append(eigVects[:, sorted_indices])
+
         return eigenvalues, eigenvectors
 
     def pick_filters(self, eigenvectors):
+        """
+        Select CSP filters based on the sorted eigenvectors.
+
+        Parameters:
+        -----------
+        eigenvectors : list of 2D arrays
+            List of eigenvectors corresponding to each pairwise combination of covariance matrices.
+
+        Returns:
+        --------
+        None
+            Updates the `filters` attribute with the selected CSP filters.
+
+        """
         filters = []
+
+        # Iterate over each set of eigenvectors
         for EigVects in eigenvectors:
+            # If filters is empty, directly assign the first set of eigenvectors
             if filters == []:
                 filters = EigVects[:, :self.n_components]
             else:
+                # Concatenate the current set of eigenvectors to the existing filters
                 filters = np.concatenate([filters, EigVects[:, :self.n_components]], axis=1)
+
+        # Transpose the filters matrix and store it in the `filters` attribute
         self.filters = filters.T
 
     def fit(self, X, y):
